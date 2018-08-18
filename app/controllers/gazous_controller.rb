0dd63a141a26691d58935d99742be7bd1c9aff1a
堂福大成 <>
2018-08-18 20:02:37 +0900

    Modify

--- app/controllers/gazous_controller.rb
+++ app/controllers/gazous_controller.rb
@@ -4,8 +4,8 @@ class GazousController < ApplicationController
   end
 
   def result
-    link = params[:image_url]
-    image = Scraping.get_product(link)
+    image = params[:image]
+    # image = Scraping.get_product(link)
     require "google/cloud/vision"
     project_id = "meter-213702"
     # Instantiates a client
@@ -17,5 +17,8 @@ class GazousController < ApplicationController
     res.each do |label|
       puts label.inspect
     end
+    @point = [560000, 89000, 0, 322900, 3499000, 11120000, 20000, 424999][rand(8)]
+    @comment = ["あーれー写真てしってるかな、君面白いね","君には自分で考えるという能力がなのかな人間やめて見ようか","他人に合わせることしかできないのかな。それ、なんていうか知ってる金魚の糞て言うんだよ","あーやっちまってんな写真なめてんの、君にはスマホすら必要ないんじゃないかな","あーあーダメだこりゃ、世紀末だわ"][rand(5)]
+    @logs = [{a:10, b:"田中武蔵"},{a:12, b:"太郎"},{a:39,b:"ロビンソン山田"},{a:22,b:"駒沢よしお"},{a:28,b:"東山典孝"},{a:33,b:"がが"},{a:45,b:"村山金井"}]
   end
 end
--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -2,6 +2,6 @@ class Scraping
   def self.get_product(link)
     agent = Mechanize.new
     page = agent.get(link)
-    image_url = page.at('.entry-content img')[:src] if page.at('.KLB4h src')
+    image_url = page.at('.KL4Bh img')[:src] if page.at('.KL4Bh img')
   end
 end
